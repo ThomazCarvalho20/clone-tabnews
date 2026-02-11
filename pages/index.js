b78a32817f71735a8ce52230a0860f30b82334b0
Thomaz Carvalho <thomazcarvalho.tc@gmail.com>
2026-02-10 22:14:30 -0300

    adicionando pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>Olá, TabNews!</h1>
+      <h2>Hello Word</h2>
+      <p>Este é o conteúdo da página inicial.</p>
+      <h1>Só modificando para o curso</h1>
+      
+    </div>
+  )
+}
+
+export default Home;
