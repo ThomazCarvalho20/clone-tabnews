5ddd67e9484d02f58727f6e4ff0a2e269e846664
ThomazCarvalho20 <thomazcarvalho.tc@gmail.com>
2026-08-22 18:30:50 -0300

    Adiciona imagem na home

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return(
         <div>
             <h1>Home</h1>
-            <img src="/images/foto.png" alt="amor da minha vida" />
+            <img src="/public/foto.png" alt="amor da minha vida" />
         </div>
     )
 }
