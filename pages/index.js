fffaf77638fd5099d63854ff9bc56fc26e328bd3
ThomazCarvalho20 <thomazcarvalho.tc@gmail.com>
2026-08-22 18:24:46 -0300

    nova atualizacao

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return(
         <div>
             <h1>Home</h1>
-            <img src="/page.png" alt="Amor da minha vida" />
+            <img src="/images/foto.png" alt="amor da minha vida" />
         </div>
     )
 }
