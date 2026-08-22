5f69b1cd8096cb0a5a137345370404bd4fdd0122
ThomazCarvalho20 <thomazcarvalho.tc@gmail.com>
2026-08-22 18:39:19 -0300

    arrumando estutura para curso

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home(){
     return(
         <div>
-            <h1>My love</h1>
-            <img src="/images/image.png" alt="amor da minha vida" width="300" height="200"/>
+            <h1>Hello World</h1>
         </div>
     )
 }
